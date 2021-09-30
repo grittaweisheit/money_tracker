56148cb6958da6101aa9f7c678f4992d46b5e9ca
Gritta Weisheit <grittaweisheit@gmail.com>
2021-10-01 00:51:11 +0200

    add total pie chart

--- lib/components/statisticsTab.dart
+++ lib/components/statisticsTab.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:money_tracker/Consts.dart';
+import 'package:money_tracker/models/Transactions.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class StatisticsTab extends StatefulWidget {
   StatisticsTab();
@@ -8,18 +12,45 @@ class StatisticsTab extends StatefulWidget {
 }
 
 class _StatisticsTabState extends State<StatisticsTab> {
+  List<OneTimeTransaction> transactions = [];
+  late PieChartData pieChartData;
+  int count = 0;
+
   @override
   void initState() {
-    super.initState();
     refresh();
+    super.initState();
   }
 
-  void refresh(){
-   // Box<Tag> box = Hive.box(tagBox);
+  refresh() {
+    Box<OneTimeTransaction> box = Hive.box(oneTimeTransactionBox);
+    List<OneTimeTransaction> newTransactions = box.values.toList();
+    newTransactions.sort((t1, t2) => t2.date.compareTo(t1.date));
+
+    PieChartSectionData incomes = PieChartSectionData(
+        color: lightGreenColor,
+        value: newTransactions.where((t) => t.isIncome).fold(
+            0, (previousValue, element) => previousValue! + element.amount));
+    PieChartSectionData expenses = PieChartSectionData(
+        color: lightRedColor,
+        value: newTransactions.where((t) => !t.isIncome).fold(
+            0, (previousValue, element) => previousValue! + element.amount));
+    PieChartData newPieChartData = PieChartData(
+        sections: [incomes, expenses], centerSpaceRadius: double.infinity);
+
+    setState(() {
+      transactions = newTransactions;
+      count = transactions.length;
+      pieChartData = newPieChartData;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Text("Statistics");
+    debugPrint('${pieChartData.sections[0].value}');
+
+    return Card(
+      child: PieChart(pieChartData),
+    );
   }
 }
